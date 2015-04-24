80d5cb18d17bacec15347698662fa5148769d7a3
sarath <chuk.a.roo@gmail.com>
2015-04-23 17:55:18 -0700

    moved sample def to header file

--- src/detection.cpp
+++ src/detection.cpp
@@ -6,37 +6,6 @@
 RNG rng(12345);
 int kernel_size = 3;
 
-/*
- * Definition of output sample
- * @ id : Id of the sample
- * @ x,y: position in world coordinates.
- */
-typedef struct
-{
-	unsigned int id;
-	double x;
-	double y;
-}DETECTED_SAMPLE;
-
-/*
- * Definition of input sample
- * @ id : Id of the sample
- * @ HSV_MIN: Min values for HCV color space
- * @ HSV_MAX: Max values for HCV color space
- * @ min and max width of the sample to search
- * @ min and max height of the sample to search
- */
-typedef struct
-{
-	unsigned int Id;
-	std::vector<int> HSV_MIN;
-	std::vector<int> HSV_MAX;
-	double min_width;
-	double max_width;
-	double min_height;
-	double max_height;
-}SAMPLE;
-
 std::vector<SAMPLE> sample;
 
 cv::Mat Input_image;
@@ -60,6 +29,8 @@ int getSampleSize()
 {
 	return sample.size();
 }
+
+
 
 bool process_image(cv::Mat image_hsv, int index)
 {    
