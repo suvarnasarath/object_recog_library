04dd76b081963616fe9bf05f1e5af86b58eac8a8
sarath <chuk.a.roo@gmail.com>
2015-04-23 17:46:32 -0700

    Changes to parameterise sample data

--- include/detection.h
+++ include/detection.h
@@ -7,3 +7,7 @@
 using namespace cv;
 
 cv::Mat find_objects(const Mat * imgPtr);
+void register_sample(unsigned int Id,
+					 std::vector<int>hsv_min, std::vector<int>hsv_max,
+					 double min_width, double max_width, double min_height, double max_height);
+int getSampleSize();
