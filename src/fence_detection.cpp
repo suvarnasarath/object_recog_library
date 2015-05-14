8c03a55a7562d93cbb083f8fecf0da668cfc3f99
sarath <chuk.a.roo@gmail.com>
2015-05-14 13:04:54 -0700

    minor updates to tune erosion/dilutio values

--- src/fence_detection.cpp
+++ src/fence_detection.cpp
@@ -9,13 +9,12 @@ int main(int argc, char **argv)
 {
 	cv::namedWindow("video",CV_WINDOW_AUTOSIZE); 
 	cv::Mat frame,src_hsv;
-	//cv::Vec3d Origin(10,127,200); 
-	cv::Vec3d Origin(170,10,200); 
+	cv::Vec3d Origin(10,127,200); 
+	//cv::Vec3d Origin(170,10,200); 
 	std::vector<cv::Mat> hsv_planes(3);
 
 	int dilation_size = 3;
 	int erosion_size = 2;
-
 
 	frame = cv::imread(argv[1],CV_LOAD_IMAGE_COLOR);
 	if(! frame.data) {
@@ -31,23 +30,20 @@ int main(int argc, char **argv)
 	cv::Mat V = hsv_planes[2]; 
 
     cv::Mat response = cv::Mat::zeros(H.rows,H.cols,CV_32FC1);
-    cv::Mat dst = cv::Mat::zeros(H.rows,H.cols,CV_32FC1);
-    cv::Mat output = cv::Mat::zeros(H.rows,H.cols,CV_32FC1);
-    cv::Mat erosion_out = cv::Mat::zeros(H.rows,H.cols,CV_32FC1);
-    cv::Mat dilation_out = cv::Mat::zeros(H.rows,H.cols,CV_32FC1);
+
+    cv::Mat dst,output,erosion_out,dilation_out;
 
     int hue_ref = Origin[0]*255/179;
     int32_t sat_ref = Origin[1];
     int32_t val_ref = Origin[2];
 
-    const int32_t MAX_SAT_DEV = 10;
+    const int32_t MAX_SAT_DEV = 120;
     const int32_t MAX_VAL_DEV = 120;
-    const int32_t MAX_HUE_DEV = 30;
+    const int32_t MAX_HUE_DEV = 50;
 
     const float HUE_MULT_FACTOR = 1.0/static_cast<double>(MAX_HUE_DEV);
     const float SAT_MULT_FACTOR = 1.0/static_cast<double>(MAX_SAT_DEV);
     const float VAL_MULT_FACTOR = 1.0/static_cast<double>(MAX_VAL_DEV);
-
 
 
 	for(int rows = 0 ;rows < frame.rows; rows++)
@@ -61,8 +57,8 @@ int main(int argc, char **argv)
 			int32_t value = static_cast<int32_t>(V.at<uint8_t>(rows,cols));
 
 			int32_t saturation_deviation = saturation - sat_ref;
-
 			int32_t value_deviation = value - val_ref;
+			int32_t hue_deviation = static_cast<int8_t>(hue_diff);
 
 			saturation_deviation = std::max(-MAX_SAT_DEV,std::min(MAX_SAT_DEV,saturation_deviation));
 			saturation_deviation = MAX_SAT_DEV - std::abs(saturation_deviation);
@@ -73,16 +69,12 @@ int main(int argc, char **argv)
 			value_deviation = MAX_VAL_DEV - std::abs(value_deviation);
 			float val_factor = VAL_MULT_FACTOR * value_deviation;
 
-			int32_t hue_deviation = static_cast<int8_t>(hue_diff);
 			hue_deviation = std::max(-MAX_HUE_DEV,std::min(MAX_HUE_DEV,hue_deviation));
 			hue_deviation = MAX_HUE_DEV - std::abs(hue_deviation);
 			float hue_factor = HUE_MULT_FACTOR * hue_deviation;
 
-
-			//float hue_factor = static_cast<float>(1.0/128.0)*(128 - std::abs(static_cast<int8_t>(hue_diff)));
-
-			const float A1 = 0.9;
-			const float A2 = 0.1;
+			const float A1 = 0.65;
+			const float A2 = 0.35;
 			const float A3 = 0.0;
 
 			float response_value = hue_factor * A1 + sat_factor * A2 + val_factor * A3;
@@ -91,8 +83,7 @@ int main(int argc, char **argv)
 			response.at<float>(rows,cols) = response_value;
 		}
 	}		
-
-	//cv::imwrite("hue_out.png",response);
+	
 	cv::Mat erosion_element = getStructuringElement( 0,cv::Size( 2*erosion_size + 1, 2*erosion_size+1),
                                        cv::Point( erosion_size, erosion_size ) );
  
@@ -101,6 +92,7 @@ int main(int argc, char **argv)
   	cv::Mat dilation_element = getStructuringElement( 0,cv::Size( 2*dilation_size + 1, 2*dilation_size+1),
                                        cv::Point( dilation_size, dilation_size ) );
   	cv::dilate( erosion_out, dilation_out, dilation_element );
+
 	cv::GaussianBlur(dilation_out, dst, cv::Size(31,31), 200.0, 200.0);
 	cv::threshold(dst,output,0.7,1.0,cv::THRESH_BINARY);
 	cv::imshow("video",output);
