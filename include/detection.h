9477fcaa1975c65aff4e5e31a206959753700b73
hhony <hans.hony@gmail.com>
2015-06-04 12:01:36 +0000

    adding cuda reference

--- include/detection.h
+++ include/detection.h
@@ -5,8 +5,14 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
+#if (CUDA_GPU)
+#include "opencv2/gpu/gpu.hpp"
+#endif
 #include <iostream>
 #include <vector>
+#include <sys/time.h>
+
+#define MAX_CAMERAS_SUPPORTED (1)
 
 typedef enum
 {
