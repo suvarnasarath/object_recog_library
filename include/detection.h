a4348c9b0f8dc51137fb744d9a0f2bc51f543d59
hhony <hans.hony@gmail.com>
2015-06-06 22:41:26 -0400

    revert to d4a945c, to continue forward

--- include/detection.h
+++ include/detection.h
@@ -5,9 +5,6 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
-#if (CUDA_GPU)
-#include "opencv2/gpu/gpu.hpp"
-#endif
 #include <iostream>
 #include <vector>
 #include <sys/time.h>
