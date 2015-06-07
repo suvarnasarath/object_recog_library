5fcaa95d033494e5e871b60cdf85d44a3b8e9703
hhony <hans.hony@gmail.com>
2015-06-07 05:39:27 +0000

    fixing a mistaken deletion

--- include/detection.h
+++ include/detection.h
@@ -5,6 +5,9 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
+#if (CUDA_GPU)
+#include "opencv2/gpu/gpu.hpp"
+#endif
 #include <iostream>
 #include <vector>
 #include <sys/time.h>
